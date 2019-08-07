0c9c940dd556a37d339e7227f962613e188737ab
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2019-08-07 00:12:19 -0300

    Add initial project

---
+++ client-interest-rate-service/src/main/java/com/lucasambrosi/app/ClientInterestRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lucasambrosi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientInterestRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientInterestRateServiceApplication.class, args);
+	}
+
+}
---
+++ client-interest-rate-service/src/test/java/com/lucasambrosi/app/ClientInterestRateServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucasambrosi.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientInterestRateServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
